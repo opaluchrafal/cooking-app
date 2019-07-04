5d6312e9da9ad89c1587a9310adde5ea080f2813
id github: opaluchrafal <opaluchrafal@gmail.com>
2019-07-04 21:41:34 +0200

    prepare project structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply  test', 'https://images.media-allrecipes.com/images/56589.png'),
+    new Recipe('A test Recipe', 'This is simply  test', 'https://images.media-allrecipes.com/images/56589.png')
+  ];
 
   constructor() { }
 
